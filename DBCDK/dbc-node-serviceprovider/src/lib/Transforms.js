db246e62ca7d1b7131812a1c425b6fc90096805f
Earl Barr <E.Barr@ucl.ac.uk>
2016-08-22 16:47:25 +0100

    annotation

--- DBCDK/dbc-node-serviceprovider/src/lib/Transforms.js
+++ DBCDK/dbc-node-serviceprovider/src/lib/Transforms.js
@@ -1,13 +1,15 @@
 'use strict';
-
 /**
  * @file
  * Module for registering all transforms
  */
-
-import {isArray} from 'lodash';
-import {now} from './Utils.js';
-
+/// <reference path="lodash.d.ts" />
+//import {isArray} from 'lodash';
+//import {now} from './Utils.js';
+function isArray(x) { }
+;
+function now() { }
+;
 /**
  * Validate the transform object.
  *
@@ -17,20 +19,17 @@ import {now} from './Utils.js';
  * @api public
  */
 function validateTransform(transform) {
-  if (!transform.event) {
-    throw new Error('No event method not found on transform');
-  }
-
-  if (!transform.requestTransform) {
-    throw new Error('No requestTransform method not found on transform');
-  }
-
-  if (!transform.responseTransform) {
-    throw new Error('No responseTransform method not found on transform');
-  }
-  return transform;
+    if (!transform.event) {
+        throw new Error('No event method not found on transform');
+    }
+    if (!transform.requestTransform) {
+        throw new Error('No requestTransform method not found on transform');
+    }
+    if (!transform.responseTransform) {
+        throw new Error('No responseTransform method not found on transform');
+    }
+    return transform;
 }
-
 /**
  *
  * @param transform
@@ -38,59 +37,51 @@ function validateTransform(transform) {
  * @param logger
  * @returns {trigger}
  */
-export default function Transform(transform, clients, logger = console) {
-  validateTransform(transform);
-  transform.clients = clients;
-  transform.logger = logger;
-
-  /**
-   *
-   * @deprecated
-   * Call the clients directly through the clients object. this.clients.clientName.method(params);
-   *
-   * @param client
-   * @param method
-   * @param params
-   * @returns {*}
-   */
-  transform.callServiceClient = function callServiceClient(client, method, params) {
-    return clients[client][method](params);
-  };
-
-  /**
-   * Trigger request on a transform
-   *
-   * @param params
-   * @param context
-   * @param callback
-   */
-  transform.trigger = function trigger(params, context) {
-    const requestStart = now();
-    const event = transform.event();
-    const request = transform.requestTransform(event, params, context);
-    const requests = isArray(request) && request || [request];
-    return requests.map((requestPromise) => {
-      return requestPromise
-        .then((response) => {
-          const transformedResponse = transform.responseTransform(response, params, context);
-          const requestStop = now();
-          logger.log('info', 'Transform has been triggered', {
-            event: event,
-            timing: requestStop - requestStart,
-            request: params,
-            /* Do not log `response` objects
-             * as these sometimes include large data,
-             * - especially with mobilsoeg-profile transforms -
-             * which has a bad performance impact
-             * when the logger tries to serialise it.
-            //serviceReponse: response,
-            //finalResponse: transformedResponse
-             */
-          });
-          return transformedResponse;
+function Transform(transform, clients, logger) {
+    if (logger === void 0) { logger = console; }
+    validateTransform(transform);
+    transform.clients = clients;
+    transform.logger = logger;
+    /**
+     *
+     * @deprecated
+     * Call the clients directly through the clients object. this.clients.clientName.method(params);
+     *
+     * @param client
+     * @param method
+     * @param params
+     * @returns {*}
+     */
+    transform.callServiceClient = function callServiceClient(client, method, params) {
+        return clients[client][method](params);
+    };
+    /**
+     * Trigger request on a transform
+     *
+     * @param params
+     * @param context
+     * @param callback
+     */
+    transform.trigger = function trigger(params, context) {
+        var requestStart = now();
+        var event = transform.event();
+        var request = transform.requestTransform(event, params, context);
+        var requests = isArray(request) && request || [request];
+        return requests.map(function (requestPromise) {
+            return requestPromise
+                .then(function (response) {
+                var transformedResponse = transform.responseTransform(response, params, context);
+                var requestStop = now();
+                logger.log('info', 'Transform has been triggered', {
+                    event: event,
+                    timing: requestStop - requestStart,
+                    request: params
+                });
+                return transformedResponse;
+            });
         });
-    });
-  };
-
-  return transform;
+    };
+    return transform;
 }
+exports.__esModule = true;
+exports["default"] = Transform;
